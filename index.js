1a1075642b2587ffe4a2e1de1382a2e5e4c7f7ed
Archil Bolkvadze <a.bolkvadze@gmail.com>
2014-11-10 23:13:35 +0400

    collect results

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
-var express = require('express')
+var express = require('express');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
+  response.send('Hello World!');
+});
+
+app.get('/results', function(request, response) {
+  var data = require('fs').readFileSync('results.json', 'utf8');
+  response.send(data);
+});
+app.post('/results', function(request, response) {
+  var fs = require('fs');
+  request.pipe(fs.createWriteStream('results.json', {flags:'a'}))
+    .once('finish', function(){
+      response.send(201);
+    });
+});
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
